f318e66762f558a68610ecb87a1866cb56be7a84
MaheshaMuthumala <31380743+MaheshaMuthumala@users.noreply.github.com>
2019-06-21 03:10:50 +0530

    Signup php

--- signup_user.php
+++ signup_user.php
@@ -14,6 +14,32 @@
         $country = htmlentities(mysqli_real_escape_string($con, $_POST['user_country']));
         $gender = htmlentities(mysqli_real_escape_string($con, $_POST['user_gender']));
         $rand = rand(1, 2);
+
+        if ($name==""){
+            echo "<script>alert('We cannot verify your name')</script>";
+        }
+        if (strlen($pass)<8){
+            echo "<script>alert('Password should be minimum 8 characters!')</script>";
+            exit();
+        }
+
+        $check_email = "select * from users where user_email='$email'";
+        $run_email = mysqli_query($con, $check_email);
+
+        $check = mysqli_num_rows($run_email);
+
+        if ($check==1){
+            echo "<script>alert('Email already exist, please try again!')</script>";
+            echo "<script>window.open('signup.php', '_self')</script>";
+            exit();
+        }
+
+        if ($rand==1)
+            $profile_pic = "images/avatar1.jpg";
+        elseif ($rand==2)
+            $profile_pic ="images/avatar2.jpg";
+
+        $insert = "insert into users (user)";
     }
 
 ?>
